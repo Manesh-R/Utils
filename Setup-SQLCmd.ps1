6b9bbf86a6d5796dfe8dce7e6d8dc68c378701e3
Manesh Raveendran <Manesh-R@users.noreply.github.com>
2020-05-14 02:52:10 +0000

    Update Setup-SQLCmd.ps1

--- Setup-SQLCmd.ps1
+++ Setup-SQLCmd.ps1
@@ -1,7 +1,7 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Manesh-R/Utils/master/VC_redist.x64.exe" -OutFile "C:\LabFiles\01.VC_redist.x64.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Manesh-R/Utils/master/VC_redist.x64.exe" -OutFile "C:\01.VC_redist.x64.exe"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Manesh-R/Utils/master/msodbcsql.msi" -OutFile "C:\LabFiles\02.msodbcsql.msi"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Manesh-R/Utils/master/msodbcsql.msi" -OutFile "C:\02.msodbcsql.msi"
 
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Manesh-R/Utils/master/MsSqlCmdLnUtils.msi" -OutFile "C:\LabFiles\03.MsSqlCmdLnUtils.msi"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Manesh-R/Utils/master/MsSqlCmdLnUtils.msi" -OutFile "C:\03.MsSqlCmdLnUtils.msi"
 
 
